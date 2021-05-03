f38ff460e47f6c1bad9874db6f91d67b0ab5de5d
Karol <2004karol@gmail.com>
2021-05-03 23:40:24 +0200

    [cli] Make fetch implementation a bit less complex

--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -16,72 +16,68 @@ func Fetch(tableName, startEndRange string) error {
 		return err
 	}
 
+	startEndRange = strings.TrimPrefix(startEndRange, "[")
+	startEndRange = strings.TrimSuffix(startEndRange, "]")
+
+	sliceStartEndRange := strings.SplitN(startEndRange, ":", 2)
+
+	billion := 1000000000
+	if sliceStartEndRange[0] == "" {
+		sliceStartEndRange[0] = "0"
+	}
+	if sliceStartEndRange[1] == "" {
+		sliceStartEndRange[1] = strconv.Itoa(billion - 1)
+	}
+
+	intStartRange, err := strconv.Atoi(sliceStartEndRange[0])
+	if err != nil {
+		return errors.New("Start range is not a number")
+	}
+	intEndRange, err := strconv.Atoi(sliceStartEndRange[1])
+	if err != nil {
+		return errors.New("End range is not a number")
+	}
+
 	var query string
-	if startEndRange == ":" {
-		query = "SELECT command, output FROM `" + tableName + "`;"
-	} else {
-		startEndRange = strings.TrimPrefix(startEndRange, "[")
-		startEndRange = strings.TrimSuffix(startEndRange, "]")
-
-		startEndRange := strings.SplitN(startEndRange, ":", 2)
-
-		billion := 1000000000
-		if startEndRange[0] == "" {
-			startEndRange[0] = "0"
-		}
-		if startEndRange[1] == "" {
-			startEndRange[1] = strconv.Itoa(billion - 1)
-		}
-
-		intStartRange, err := strconv.Atoi(startEndRange[0])
-		if err != nil {
-			return errors.New("Start range is not a number")
-		}
-		intEndRange, err := strconv.Atoi(startEndRange[1])
-		if err != nil {
-			return errors.New("End range is not a number")
-		}
-
-		if intStartRange < billion && intEndRange < billion {
-			if intStartRange < 0 && intEndRange < 0 {
-				query = fmt.Sprintf("SELECT command, output FROM `%s`"+
-					"WHERE rowid >= (SELECT max(rowid)+%s FROM `%s`) AND rowid < (SELECT max(rowid)+%s FROM `%s`);",
-					tableName, startEndRange[0], tableName, startEndRange[1], tableName)
-			} else if intStartRange < 0 {
-				query = fmt.Sprintf("SELECT command, output FROM `%s`"+
-					"WHERE rowid >= (SELECT max(rowid)+%s FROM `%s`) AND rowid <= %s;",
-					tableName, startEndRange[0], tableName, startEndRange[1])
-			} else if intEndRange < 0 {
-				query = fmt.Sprintf("SELECT command, output FROM `%s`"+
-					"WHERE rowid > %s AND rowid < (SELECT max(rowid)+%s FROM `%s`);",
-					tableName, startEndRange[0], startEndRange[1], tableName)
-			} else {
-				query = fmt.Sprintf("SELECT command, output FROM `%s`"+
-					"WHERE rowid > %s AND rowid <= %s;",
-					tableName, startEndRange[0], startEndRange[1])
-			}
-		} else if intStartRange < billion {
-			if intStartRange < 0 {
-				query = fmt.Sprintf("SELECT command, output FROM `%s`"+
-					"WHERE rowid >= (SELECT max(rowid)+%s FROM `%s`) AND indicator <= %s;",
-					tableName, startEndRange[0], tableName, startEndRange[1])
-			} else {
-				query = fmt.Sprintf("SELECT command, output FROM `%s` WHERE rowid > %s AND indicator <= %s;",
-					tableName, startEndRange[0], startEndRange[1])
-			}
-		} else if intEndRange < billion {
-			if intEndRange < 0 {
-				query = fmt.Sprintf("SELECT command, output FROM `%s`"+
-					"WHERE indicator >= %s AND rowid < (SELECT max(rowid)+%s FROM `%s`);",
-					tableName, startEndRange[0], startEndRange[1], tableName)
-			} else {
-				query = fmt.Sprintf("SELECT command, output FROM `%s` WHERE indicator >= %s AND rowid <= %s;",
-					tableName, startEndRange[0], startEndRange[1])
-			}
+	if intStartRange < billion && intEndRange < billion {
+		if intStartRange < 0 && intEndRange < 0 {
+			query = fmt.Sprintf("SELECT command, output FROM `%s`"+
+				"WHERE rowid >= (SELECT max(rowid)+%s FROM `%s`) AND rowid < (SELECT max(rowid)+%s FROM `%s`);",
+				tableName, sliceStartEndRange[0], tableName, sliceStartEndRange[1], tableName)
+		} else if intStartRange < 0 {
+			query = fmt.Sprintf("SELECT command, output FROM `%s`"+
+				"WHERE rowid >= (SELECT max(rowid)+%s FROM `%s`) AND rowid <= %s;",
+				tableName, sliceStartEndRange[0], tableName, sliceStartEndRange[1])
+		} else if intEndRange < 0 {
+			query = fmt.Sprintf("SELECT command, output FROM `%s`"+
+				"WHERE rowid > %s AND rowid < (SELECT max(rowid)+%s FROM `%s`);",
+				tableName, sliceStartEndRange[0], sliceStartEndRange[1], tableName)
 		} else {
-			query = fmt.Sprintf("SELECT command, output FROM `%s` WHERE indicator >= %s AND indicator <= %s;",
-				tableName, startEndRange[0], startEndRange[1])
+			query = fmt.Sprintf("SELECT command, output FROM `%s`"+
+				"WHERE rowid > %s AND rowid <= %s;",
+				tableName, sliceStartEndRange[0], sliceStartEndRange[1])
 		}
+	} else if intStartRange < billion {
+		if intStartRange < 0 {
+			query = fmt.Sprintf("SELECT command, output FROM `%s`"+
+				"WHERE rowid >= (SELECT max(rowid)+%s FROM `%s`) AND indicator <= %s;",
+				tableName, sliceStartEndRange[0], tableName, sliceStartEndRange[1])
+		} else {
+			query = fmt.Sprintf("SELECT command, output FROM `%s` WHERE rowid > %s AND indicator <= %s;",
+				tableName, sliceStartEndRange[0], sliceStartEndRange[1])
+		}
+	} else if intEndRange < billion {
+		if intEndRange < 0 {
+			query = fmt.Sprintf("SELECT command, output FROM `%s`"+
+				"WHERE indicator >= %s AND rowid < (SELECT max(rowid)+%s FROM `%s`);",
+				tableName, sliceStartEndRange[0], sliceStartEndRange[1], tableName)
+		} else {
+			query = fmt.Sprintf("SELECT command, output FROM `%s` WHERE indicator >= %s AND rowid <= %s;",
+				tableName, sliceStartEndRange[0], sliceStartEndRange[1])
+		}
+	} else {
+		query = fmt.Sprintf("SELECT command, output FROM `%s` WHERE indicator >= %s AND indicator <= %s;",
+			tableName, sliceStartEndRange[0], sliceStartEndRange[1])
 	}
 
 	rows, err := db.Query(query)
@@ -104,6 +100,7 @@ func Fetch(tableName, startEndRange string) error {
 		content.WriteString(command)
 		content.WriteString(output)
 	}
+
 	fmt.Print(content.String())
 	return nil
 }
