ff845ba71b53cb4b86c98504f080e26e1d00ee3e
Karol <2004karol@gmail.com>
2021-08-25 18:45:51 +0200

    [lib] Move content of version.c to nhi.c

--- lib/src/nhi.c
+++ lib/src/nhi.c
@@ -46,6 +46,8 @@ void *get_data_from_other_process(pid_t, size_t, void *);
 
 void destroy(void);
 
+size_t fwrite(const void *restrict, size_t, size_t, FILE *restrict);
+
 pid_t fork(void);
 
 int execve(const char *, char *const [], char *const []);
@@ -208,7 +210,22 @@ __attribute__((destructor)) void destroy(void)
   }
 }
 
-#include "bash/version.c"
+// It looks like fwrite in bash is used first time for writing prompt. At this point terminal is set up.
+size_t fwrite(const void *restrict ptr, size_t size, size_t nitems, FILE *restrict stream)
+{
+  size_t (*original_fwrite)() = (size_t (*)())dlsym(RTLD_NEXT, "fwrite");
+  size_t result = original_fwrite(ptr, size, nitems, stream);
+  if (is_bash && !is_terminal_setup) {
+    add_PS1(socket_fd, getenv("NHI_PS1"));
+
+    setenv("NHI_PROMPTER_PID", prompter_pid_str, 1);
+
+    *environ_pointer = &environ;
+
+    is_terminal_setup = true;
+  }
+  return result;
+}
 
 pid_t fork(void)
 {
