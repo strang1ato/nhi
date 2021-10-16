c6d3d2298be976c1c3b4c7f96dad85921e621b78
strang1ato <2004karol@gmail.com>
2021-10-16 16:29:59 +0200

    [cli] Move part of fetch logic to other functions

--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -12,57 +12,92 @@ import (
 
 // Fetch retrieves shell session optionally with given range of commands
 func Fetch(db *sql.DB, indicator, startEndRange, directory string) error {
+	billion := 1000000000
+	sliceStartEndRange, err := getSliceStartEndRange(startEndRange, billion)
+	if err != nil {
+		return err
+	}
+
+	startRangeInt, endRangeInt, err := getStartAndEndRangeInt(sliceStartEndRange)
+	if err != nil {
+		return err
+	}
+
+	where := getWhere(sliceStartEndRange, startRangeInt, endRangeInt, billion, indicator, directory)
+
+	query := fmt.Sprintf("SELECT PS1, command, output FROM `%s` WHERE %s;", indicator, where)
+
+	rows, err := db.Query(query)
+	if err != nil {
+		if err.Error() == "no such table: "+indicator {
+			return errors.New("no such shell session: " + indicator)
+		}
+		return err
+	}
+
+	if err := printRows(db, rows); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getSliceStartEndRange(startEndRange string, billion int) ([]string, error) {
 	startEndRange = strings.TrimPrefix(startEndRange, "{")
 	startEndRange = strings.TrimSuffix(startEndRange, "}")
 
 	sliceStartEndRange := strings.SplitN(startEndRange, ":", 2)
 
-	if (len(sliceStartEndRange) < 2) {
-		return errors.New("Please specify range of commands properly")
+	if len(sliceStartEndRange) < 2 {
+		return nil, errors.New("Please specify range of commands properly")
 	}
 
-	billion := 1000000000
 	if sliceStartEndRange[0] == "" {
 		sliceStartEndRange[0] = "0"
 	}
 	if sliceStartEndRange[1] == "" {
 		sliceStartEndRange[1] = strconv.Itoa(billion - 1)
 	}
+	return sliceStartEndRange, nil
+}
 
-	intStartRange, err := strconv.Atoi(sliceStartEndRange[0])
+func getStartAndEndRangeInt(sliceStartEndRange []string) (int, int, error) {
+	startRangeInt, err := strconv.Atoi(sliceStartEndRange[0])
 	if err != nil {
-		return errors.New("Start range is not a number")
+		return 0, 0, errors.New("Start range is not a number")
 	}
-	intEndRange, err := strconv.Atoi(sliceStartEndRange[1])
+	endRangeInt, err := strconv.Atoi(sliceStartEndRange[1])
 	if err != nil {
-		return errors.New("End range is not a number")
+		return 0, 0, errors.New("End range is not a number")
 	}
+	return startRangeInt, endRangeInt, nil
+}
 
+func getWhere(sliceStartEndRange []string, startRangeInt, endRangeInt, billion int, indicator, directory string) string {
 	var where string
-	if intStartRange < billion && intEndRange < billion {
-		if intStartRange < 0 && intEndRange < 0 {
+	if startRangeInt < billion && endRangeInt < billion {
+		if startRangeInt < 0 && endRangeInt < 0 {
 			where = fmt.Sprintf("rowid >= (SELECT max(rowid)+%s FROM `%s`) AND rowid < (SELECT max(rowid)+%s FROM `%s`)",
 				sliceStartEndRange[0], indicator, sliceStartEndRange[1], indicator)
-		} else if intStartRange < 0 {
+		} else if startRangeInt < 0 {
 			where = fmt.Sprintf("rowid >= (SELECT max(rowid)+%s FROM `%s`) AND rowid <= %s",
 				sliceStartEndRange[0], indicator, sliceStartEndRange[1])
-		} else if intEndRange < 0 {
+		} else if endRangeInt < 0 {
 			where = fmt.Sprintf("rowid > %s AND rowid < (SELECT max(rowid)+%s FROM `%s`)",
 				sliceStartEndRange[0], sliceStartEndRange[1], indicator)
 		} else {
 			where = fmt.Sprintf("rowid > %s AND rowid <= %s",
 				sliceStartEndRange[0], sliceStartEndRange[1])
 		}
-	} else if intStartRange < billion {
-		if intStartRange < 0 {
+	} else if startRangeInt < billion {
+		if startRangeInt < 0 {
 			where = fmt.Sprintf("rowid >= (SELECT max(rowid)+%s FROM `%s`) AND indicator <= %s",
 				sliceStartEndRange[0], indicator, sliceStartEndRange[1])
 		} else {
 			where = fmt.Sprintf("rowid > %s AND indicator <= %s",
 				sliceStartEndRange[0], sliceStartEndRange[1])
 		}
-	} else if intEndRange < billion {
-		if intEndRange < 0 {
+	} else if endRangeInt < billion {
+		if endRangeInt < 0 {
 			where = fmt.Sprintf("indicator >= %s AND rowid < (SELECT max(rowid)+%s FROM `%s`)",
 				sliceStartEndRange[0], sliceStartEndRange[1], indicator)
 		} else {
@@ -80,17 +115,10 @@ func Fetch(db *sql.DB, indicator, startEndRange, directory string) error {
 		}
 		where = fmt.Sprintf("%s AND pwd = '%s'", where, directory)
 	}
+	return where
+}
 
-	query := fmt.Sprintf("SELECT PS1, command, output FROM `%s` WHERE %s;", indicator, where)
-
-	rows, err := db.Query(query)
-	if err != nil {
-		if err.Error() == "no such table: "+indicator {
-			return errors.New("no such shell session: " + indicator)
-		}
-		return err
-	}
-
+func printRows(db *sql.DB, rows *sql.Rows) error {
 	for rows.Next() {
 		var PS1, command string
 		var output []byte
